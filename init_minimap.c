37cc21abee84dc33501569d80ec5cd6bb090e1a9
textualbois <wivna360@hotmail.com>
2025-01-24 00:32:39 +0100

    minimap init

---
+++ init_minimap.c
@@ -0,0 +1,34 @@
+#include "cub3d.h"
+
+mlx_image_t *init_minimap(mlx_t *window, int map[])
+{
+	mlx_image_t *minimap;
+	int x = 0;
+	int y = 0;
+	int mHeight = 8;
+	int mWidth = 8;
+	// uint32_t color = 0;
+
+	minimap = mlx_new_image(window, WIDTH, HEIGHT);
+	while (x < WIDTH)
+	{
+		y = 0;
+		while (y < HEIGHT)
+		{
+			if (x % (WIDTH / mWidth) == 0 || y % (HEIGHT / mHeight) <= 1)
+				mlx_put_pixel(minimap, x, y, 0x003333FF); // if grid
+			else
+			{
+				if (map[(int)(y / (HEIGHT / mHeight)) * mWidth + (int)(x / (WIDTH / mWidth))] == 1)
+					mlx_put_pixel(minimap, x, y, 0xFF0000FF); // if wall
+				else
+					mlx_put_pixel(minimap, x, y, 0xFFFFAAFF); // if empty
+			}
+			y++;
+		}
+		x++;
+	}
+	mlx_image_to_window(window, minimap, 0, 0);
+
+	return (minimap);
+}
