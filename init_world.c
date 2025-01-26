c6504bb5a61613cc239bc73375bf7ae25e4a5357
textualbois <wivna360@hotmail.com>
2025-01-26 05:01:43 +0100

    refactor world init

--- init_world.c
+++ init_world.c
@@ -8,31 +8,33 @@ t_World_Controller *init_world(int map[])
 	if (!world)
 		return (NULL);
 	world->window = initialize_window();
-	world->minimap = init_minimap(world->window, map);
-	world->player = init_mini_charachter(world->window, (t_IntPair){5, 5}, (t_IntPair){HEIGHT / 2, WIDTH / 2});
-	
+	if (!world->window)
+	{
+		free(world);
+		return (NULL);
+	}
+	world->player = init_mini_character((t_IntPair){5, 5}, (t_IntPair){HEIGHT / 2, WIDTH / 2});
 	if (!world->player)
 	{
 		free(world);
 		return (NULL);
 	}
-	world->frameCounter = init_frame_counter();
-	if (!world->frameCounter)
+
+	if(!init_images(world, map))
 	{
 		free(world->player);
 		free(world);
 		return (NULL);
 	}
+
+
+	// world->frameCounter = init_frame_counter();
+	// if (!world->frameCounter)
+	// {
+	// 	free(world->player);
+	// 	free(world);
+	// 	return (NULL);
+	// }
 	return (world);
 }
 
-mlx_t* initialize_window(void)
-{
-	mlx_t *window;
-
-	printf("initing window\n");
-	window = mlx_init(WIDTH, HEIGHT, "Cubisimo", true);
-	if (!window)
-		puts(mlx_strerror(mlx_errno));
-	return (window);
-}
--- world.h
+++ world.h
@@ -1,16 +1,24 @@
 #ifndef WORLD_H
 # define WORLD_H
 
-#include "cub3d.h"
+#include "MLX42/MLX42.h"
+#include "character.h"
 #include "fps.h"
+#include "window.h"
+#include "mini_map.h"
 
 typedef struct s_World_Controller{
 	mlx_t			*window;
 	t_character		*player;
 	mlx_image_t		*minimap;
-	t_FrameCounter	*frameCounter;
+	mlx_image_t		*miniCharacter;
+	//t_FrameCounter	*frameCounter;
 }	t_World_Controller;
 
-t_World_Controller *init_world(int map[]);
+t_World_Controller	*init_world(int map[]);
+
+int					init_images(t_World_Controller *world, int map[]);
+mlx_image_t			*init_image_mini_character(mlx_t *window, t_character *player, t_IntPair size);
+mlx_image_t			*init_image_minimap(mlx_t *window, int map[]);
 
 #endif
