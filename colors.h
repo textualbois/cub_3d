61fd5bd4e80f39ded2924d0d698c12b38c6cb4b8
textualbois <wivna360@hotmail.com>
2025-01-29 07:22:36 +0100

    template color definintions

---
+++ colors.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 07:21:30 by isemin            #+#    #+#             */
+/*   Updated: 2025/01/29 07:22:17 by isemin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define BLACK 0x00000000
+# define WHITE 0x00FFFFFF
+# define RED 0x00FF0000
+# define GREEN 0x0000FF00
+# define BLUE 0x000000FF
+# define YELLOW 0x00FFFF00
+# define CYAN 0x0000FFFF
+# define MAGENTA 0x00FF00FF
+
+#endif
