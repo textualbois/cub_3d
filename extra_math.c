ccb3275cecbdab7691161e4d934dafb2db8bdc54
textualbois <wivna360@hotmail.com>
2025-01-29 09:45:10 +0100

    some extra math functions

---
+++ extra_math.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extra_math.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 09:41:00 by isemin            #+#    #+#             */
+/*   Updated: 2025/01/29 09:44:29 by isemin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "extra_math.h"
+
+double	distance(t_DoublePair a, t_DoublePair b)
+{
+	return (sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)));
+}
+
+double	ceil_map(double x)
+{
+	return (((int)x / TILE_SIZE + 1) * TILE_SIZE);
+}
+
+double	floor_map(double x)
+{
+	return (((int)x / TILE_SIZE) * TILE_SIZE);
+}
---
+++ extra_math.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extra_math.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 09:42:08 by isemin            #+#    #+#             */
+/*   Updated: 2025/01/29 09:42:23 by isemin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXTRA_MATH_H
+# define EXTRA_MATH_H
+
+# include "types_numeric.h"
+
+float	distance(t_DoublePair a, t_DoublePair b);
+float	ceil_map(float x);
+float	floor_map(float x);
