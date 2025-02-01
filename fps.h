ea8dc2a050615ddd4dd6941d7cc82463c1131585
textualbois <wivna360@hotmail.com>
2025-02-01 22:00:25 +0100

    place holder for fps text box

--- fps.h
+++ fps.h
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 17:11:44 by isemin            #+#    #+#             */
-/*   Updated: 2025/01/26 17:12:37 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/01 21:59:57 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@
 #define INTERVALS 10
 
 typedef struct s_FrameCounter {
-	int frameCounts[INTERVALS];
-	int intervalIndex;
-	double lastUpdateTime;
+	int			frameCounts[INTERVALS];
+	int			intervalIndex;
+	double		lastUpdateTime;
+	// mlx_image_t	*text_box;
 }	t_FrameCounter;
 
 t_FrameCounter	*init_frame_counter();
+// mlx_image_t		*init_text_box(mlx_t *window);
 void			destroy_frame_counter(t_FrameCounter *frameCounter);
 void			update_frame_counter(t_FrameCounter *frameCounter);
 double			get_frame_count(t_FrameCounter *frameCounter);
