80d72fa4d03f95615af5f05175ad93258d3e2b57
textualbois <wivna360@hotmail.com>
2025-02-01 21:10:55 +0100

    moved draw_world to world.h and added map custom type to world_controller

--- world.h
+++ world.h
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 17:12:18 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/01 17:47:59 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/01 21:10:06 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ typedef struct s_World_Controller{
 	mlx_t			*window;
 	t_IntPair		size;
 	t_character		*player;
-	int				map[8][8]; // change to t_map?
+	t_mini_map		*map;
+	//int				map[8][8]; // change to t_map?
 	mlx_image_t		*minimap;
 	mlx_image_t		*miniCharacter;
 	//t_FrameCounter	*frameCounter;
@@ -36,4 +37,5 @@ int					init_images(t_World_Controller *world);//, int map[8][8]);
 mlx_image_t			*init_image_mini_character(mlx_t *window, t_character *player, t_IntPair size);
 mlx_image_t			*init_image_minimap(mlx_t *window);//, int map[8][8]);
 
+void				draw_world(t_World_Controller *world);
 #endif
