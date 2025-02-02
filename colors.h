98b6b5ddb7f34cca383bc30772ae5afee6b55983
textualbois <wivna360@hotmail.com>
2025-02-02 21:33:14 +0100

    colors still dont really work, maybe we dont need them anyway

--- colors.h
+++ colors.h
@@ -6,20 +6,20 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 07:21:30 by isemin            #+#    #+#             */
-/*   Updated: 2025/01/29 07:22:17 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/02 20:30:54 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define BLACK 0x00000000
-# define WHITE 0x00FFFFFF
-# define RED 0x00FF0000
-# define GREEN 0x0000FF00
-# define BLUE 0x000000FF
-# define YELLOW 0x00FFFF00
-# define CYAN 0x0000FFFF
-# define MAGENTA 0x00FF00FF
+# define BLACK 0xFF000000
+# define WHITE 0xFFFFFFFF
+# define RED 0xFFFF0000
+# define GREEN 0xFF00FF00
+# define BLUE 0xFF0000FF
+# define YELLOW 0xFFFFFF00
+# define CYAN 0xFF00FFFF
+# define MAGENTA 0xFFFF00FF
 
 #endif
