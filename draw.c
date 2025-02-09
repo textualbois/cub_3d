0ed8ef9460683eddfbc455d8358fdf4073ed693b
vmamoten <vladyslav.mamotenko@gmail.com>
2025-02-09 12:13:21 +0100

    adjusting for test col in 2d map

--- draw.c
+++ draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 17:49:48 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/09 02:25:41 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/09 12:07:30 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	redraw(void *param)
 	// player is drawn relative to his image, so we need to move the image
 	centre_character_img(world, world->mini_map, world->player->pos); // then we move the player image box, if hes close to a border
 	// sleep(3);
-	raycasting(world); // then we draw the rays
+	// raycasting(world); // then we draw the rays
 	ft_color_mini_character(world->miniCharacter, 0xFF00FFFF);
 	// color_rays
 	//ft_color_mini_character_direction(world->miniCharacter, 0xFF0000FF, world->player); // then we draw the player
@@ -52,32 +52,32 @@ void	redraw(void *param)
 	write(1, "redraw_end\n", 11);
 }
 
-void	raycasting(t_World_Controller *world)
-{
-	printf("raycasting\n");
-	double		rayDir;
-	t_DoublePair	hit;
-	int			x;
-	double		fov_radians;
+// void	raycasting(t_World_Controller *world)
+// {
+// 	printf("raycasting\n");
+// 	double		rayDir;
+// 	t_DoublePair	hit;
+// 	int			x;
+// 	double		fov_radians;
 
-	fov_radians = FOV * PI / 180;
-	rayDir = world->player->angle - fov_radians / 2;
-	x = 0;
-	// printf("raycasting loop\n");
-	// fflush(stdout);
-	while (x < world->window->width)
-	{
-		hit = ray_find_wall(world->mini_map, world->player, rayDir);
-		// printf("raycasting loop %d out of %d hit x: %f, y: %f\n", x + 1, world->window->width, hit.x, hit.y);
+// 	fov_radians = FOV * PI / 180;
+// 	rayDir = world->player->angle - fov_radians / 2;
+// 	x = 0;
+// 	// printf("raycasting loop\n");
+// 	// fflush(stdout);
+// 	while (x < world->window->width)
+// 	{
+// 		hit = ray_find_wall(world->mini_map, world->player, rayDir);
+// 		// printf("raycasting loop %d out of %d hit x: %f, y: %f\n", x + 1, world->window->width, hit.x, hit.y);
 
-		// fflush(stdout);
-		drawray(world->player, world->map_img, world->mini_map, hit);
-		rayDir += fov_radians / world->window->width;
-		if (rayDir < 0)
-			rayDir += 2 * PI;
-		else if (rayDir >= 2 * PI)
-			rayDir -= 2 * PI;
-		x++;
-		// printf("next loop\n");
-	}
-}
+// 		// fflush(stdout);
+// 		drawray(world->player, world->map_img, world->mini_map, hit);
+// 		rayDir += fov_radians / world->window->width;
+// 		if (rayDir < 0)
+// 			rayDir += 2 * PI;
+// 		else if (rayDir >= 2 * PI)
+// 			rayDir -= 2 * PI;
+// 		x++;
+// 		// printf("next loop\n");
+// 	}
+// }
