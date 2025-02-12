c00e187a19b29077220dcdd17f8a66463eef0700
textualbois <wivna360@hotmail.com>
2025-02-12 22:44:26 +0100

    fixed rays not drawing to the end of the mini map when wall is not in view on it

--- draw_ray_mmap.c
+++ draw_ray_mmap.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 21:39:44 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/09 23:26:18 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/12 22:42:39 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ void	drawray(t_character *player, mlx_image_t *map_img, t_mini_map *mini_map, t_
 	t_IntPair	start;
 	t_IntPair	end;
 
+	if (hit.x < 0 || hit.y < 0)
+		return ;
+		// draw_line_between_pixels(map_img, start, end, 0xFFFFFFFF);
+
 	start.x = (player->pos.x - mini_map->view_port.x) / mini_map->visible_size.x * map_img->width;
 	start.y = (player->pos.y - mini_map->view_port.y) / mini_map->visible_size.y * map_img->height;
 	end.x = (hit.x - mini_map->view_port.x) / mini_map->visible_size.x * map_img->width;
 	end.y = (hit.y - mini_map->view_port.y) / mini_map->visible_size.y * map_img->height;
 	// if (map_img == NULL)
 		// printf("map_img is NULL\n");
-	if (end.x >= 0 && end.y >= 0)
-		draw_line_between_pixels(map_img, start, end, 0xFF000000);
+	// if (end.x >= 0 && end.y >= 0)
+	draw_line_between_pixels(map_img, start, end, 0xFFFFFFFF);
 	// printf("drew a line\n");
 }
 
@@ -43,12 +47,8 @@ void draw_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair end, 
 	err = d.x + d.y;
 	while (start.x != end.x || start.y != end.y)
 	{
-		if (start.x < 0 || start.x + 1 >= (int)img->width || start.y < 0 || start.y + 1 > (int)img->height)
-		{
-			printf("pixel is out of bounds x: %d, y: %d\n", start.x, start.y);
-			break;
-		}
-		mlx_put_pixel(img, start.x, start.y, color);
+		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0 && start.y < (int)img->height)
+			mlx_put_pixel(img, start.x, start.y, color);
 		e2 = 2 * err;
 		if (e2 >= d.y)
 		{
