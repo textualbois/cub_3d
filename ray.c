f4e5639033bf0a2366f5b5a0ec8208f964e2afb2
textualbois <wivna360@hotmail.com>
2025-02-16 00:59:24 +0100

    the result of a raycast is renderData object with a pre-requisite of the x value of the textures x coordinate for this exact ray/window pixel

--- ray.c
+++ ray.c
@@ -6,75 +6,41 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 06:35:51 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/15 19:38:42 by isemin           ###   ########.fr       */
+/*   Updated: 2025/02/16 00:58:16 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray.h"
 #include <stdio.h>
 
-// static void print_map_hit(t_mini_map *map, t_DoublePair hit)
-// {
-// 	int x, y;
-
-// 	y = 0;
-// 	x = 0;
-// 	printf("Y\\X");
-// 	while (x < map->size_int.x)
-// 	{
-// 		printf("|%02d|", x);
-// 		x++;
-
-// 	}
-// 	printf("\n");
-// 	x = 0;
-// 	while (x < map->size_int.x)
-// 	{
-// 		printf("____");
-// 		x++;
-// 	}
-// 	printf("\n");
-
-// 	y = 0;
-// 	while (y < map->size_int.y)
-// 	{
-// 		printf("%02d|", y);
-// 		x = 0;
-// 		while (x < map->size_int.x)
-// 		{
-// 			if (y == (int)hit.y / TILE_SIZE && x == (int)hit.x / TILE_SIZE)
-// 				printf(" [%c]", map->map[y][x]);
-// 			else
-// 				printf("  %c ", map->map[y][x]);
-// 			x++;
-// 		}
-// 		printf("\n");
-// 		y++;
-// 	}
-// 	fflush(stdout);
-// }
-
-// static int angle_to_degree(double angle)
-// {
-// 	return (angle * 180 / PI);
-// }
-
-static void ray_result(t_renderData  *data, t_DoublePair hit, int direction, int vert_hor)
+static void ray_result(t_renderData  *data, t_DoublePair hit, int direction, int hit_type)
 {
 	data->hit = hit;
-	if (vert_hor == VERTICAL)
+	if (hit_type == VERTICAL)
 	{
 		if (direction == -1)
-			data->txtr_code = NORTH;
+		{
+			data->txtr_code = WEST;
+			data->txtr.x = ceil(hit.y / TILE_SIZE) * TILE_SIZE - hit.y;
+		}
 		else
-			data->txtr_code = SOUTH;
+		{
+			data->txtr_code = EAST;
+			data->txtr.x = hit.y - floor(hit.y / TILE_SIZE) * TILE_SIZE;
+		}
 	}
 	else
 	{
 		if (direction == -1)
-			data->txtr_code = WEST;
+		{
+			data->txtr_code = NORTH;
+			data->txtr.x = hit.x - floor(hit.x / TILE_SIZE) * TILE_SIZE;
+		}
 		else
-			data->txtr_code = EAST;
+		{
+			data->txtr_code = SOUTH;
+			data->txtr.x = ceil(hit.x / TILE_SIZE) * TILE_SIZE - hit.x;
+		}
 	}
 }
 
@@ -100,11 +66,8 @@ void	ray_find_wall(t_mini_map *mini_map, t_character *player, t_renderData *data
 	t_IntPair		direction;
 
 	direction = directions(data->rayDir);
-	// printf("checking direction %f, which is %i degrees\n", rayDir, angle_to_degree(rayDir));
-
 	vertical_hit = ray_find_vertical_hit(mini_map, player, data->rayDir, direction);
 	horizontal_hit = ray_find_horizontal_hit(mini_map, player, data->rayDir, direction);
-
 	if (vertical_hit.x == -1)
 		ray_result(data, horizontal_hit, direction.y, HORIZONTAL);
 	else if (horizontal_hit.x == -1)
@@ -126,23 +89,15 @@ t_DoublePair	ray_find_vertical_hit(t_mini_map *mini_map, t_character *player, do
 {
 	t_DoublePair	hit;
 
-	// right direction
-	// if (rayDir < PI / 2 || rayDir > 3 * PI / 2) {
 	if (direction.x == 1) {
 		hit.x = ceil_map(player->pos.x);
-	} else { // left direction
+	} else {
 		hit.x = floor_map(player->pos.x);
 	}
-
-	// printf("checking direction %f, which is %i degrees\n", rayDir, angle_to_degree(rayDir));
 	hit.y = player->pos.y - (hit.x - player->pos.x) * tan(rayDir);
-
-	// return (hit);
-	// printf("pos x: %f, y: %f. hit x: %f , hit.y %f\n", player->pos.x, player->pos.y, hit.x, hit.y);
 	if (check_hit_bounds(&hit, mini_map) == false)
 		return ((t_DoublePair){-1, -1});
 	while (is_wall(mini_map, hit, direction, VERTICAL) == false) {
-		// printf("checking direction %f, which is %i degrees\n", rayDir, angle_to_degree(rayDir));
 		increment_vertical(&hit, direction.x, rayDir);
 		if (check_hit_bounds(&hit, mini_map) == false)
 			return ((t_DoublePair){-1, -1});
@@ -154,17 +109,14 @@ t_DoublePair	ray_find_horizontal_hit(t_mini_map *mini_map, t_character *player, 
 {
 	t_DoublePair	hit;
 
-	// up direction
 	if (direction.y == -1)
 		hit.y = floor_map(player->pos.y);
 	else // down direction
 		hit.y = ceil_map(player->pos.y);
 	hit.x = player->pos.x - (hit.y - player->pos.y) / tan(rayDir);
-	//return (hit);
 	if (check_hit_bounds(&hit, mini_map) == false)
 		return ((t_DoublePair){-1, -1});
 	while (is_wall(mini_map, hit, direction, HORIZONTAL) == false) {
-		// printf("checking direction %f, which is %i degrees\n", rayDir, angle_to_degree(rayDir));
 		increment_horizontal(&hit, direction.y, rayDir);
 		if (check_hit_bounds(&hit, mini_map) == false)
 			return ((t_DoublePair){-1, -1});
@@ -174,10 +126,7 @@ t_DoublePair	ray_find_horizontal_hit(t_mini_map *mini_map, t_character *player, 
 
 bool	is_wall(t_mini_map *mini_map, t_DoublePair hit, t_IntPair direction, int caller)
 {
-	// print_map_hit(mini_map, hit);
 	t_IntPair	modifier;
-	// (void)caller;
-	// (void)direction;
 	modifier.x = 0;
 	modifier.y = 0;
 	if (direction.x == 1)
@@ -188,26 +137,7 @@ bool	is_wall(t_mini_map *mini_map, t_DoublePair hit, t_IntPair direction, int ca
 		modifier.y = 0;
 	else if (caller == HORIZONTAL)
 		modifier.y = -1;
-
 	if (mini_map->map[modifier.y + (int)hit.y / TILE_SIZE][modifier.x + (int)hit.x / TILE_SIZE] == '1')
-	{
-		printf("WALL FOUND !!!\n");
 		return (true);
-	}
 	return (false);
 }
-
-// static t_IntPair directions(double rayDir)
-// {
-// 	t_IntPair	directions;
-
-// 	if (rayDir < PI / 2 || rayDir > 3 * PI / 2)
-// 		directions.x = 1;
-// 	else
-// 		directions.x = -1;
-// 	if (rayDir < PI)
-// 		directions.y = -1;
-// 	else
-// 		directions.y = 1;
-// 	return (directions);
-// }
