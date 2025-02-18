b3a49d053b5d0c1f539c14da48fa2b4b01745ce6
vmamoten <vladyslav.mamotenko@gmail.com>
2025-02-18 17:32:57 +0100

    working with norms

--- draw_mini_character.c
+++ draw_mini_character.c
@@ -6,7 +6,7 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 17:11:37 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/17 14:29:17 by vmamoten         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:51:01 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ void	ft_color_mini_character(mlx_image_t *player, int color)
 	uint32_t	y;
 
 	if (!player)
-	{
-		// printf("[ERROR] ft_color_mini_character: player is NULL\n");
 		return ;
-	}
 	x = 0;
 	while (x < player->width)
 	{
@@ -45,11 +42,6 @@ void	ft_color_mini_character_direction(mlx_image_t *character, int color,
 	t_IntPair		iter;
 	t_DoublePair	end;
 
-	if (!character || !player)
-	{
-		//	printf("[ERROR] ft_color_mini_character_direction: NULL input\n");
-		return ;
-	}
 	centre.x = (double)character->width / 2;
 	centre.y = (double)character->height / 2;
 	offset = 2;
@@ -61,25 +53,19 @@ void	ft_color_mini_character_direction(mlx_image_t *character, int color,
 		{
 			if (iter.x >= 0 && iter.x < (int)character->width && iter.y >= 0
 				&& iter.y < (int)character->height)
-			{
 				mlx_put_pixel(character, iter.x, iter.y, color);
-			}
 			iter.y++;
 		}
 		iter.x++;
 	}
 	end.x = centre.x + (player->size.x) * cos(player->angle.x);
 	end.y = centre.y - (player->size.y) * sin(player->angle.x);
-	//	printf("[DEBUG] ft_color_mini_character_direction: center=(%.2f, %.2f),
-			// end=(%.2f, %.2f)\n", centre.x, centre.y, end.x, end.y);
 	ft_color_line(character, color, centre, end);
 }
 
 void	ft_bresenham_init(t_line_params *params, t_DoublePair start,
 		t_DoublePair end)
 {
-	// printf("[DEBUG] ft_bresenham_init: start=(%.2f, %.2f), end=(%.2f,
-		// %.2f)\n", start.x, start.y, end.x, end.y);
 	params->delta.x = fabs(end.x - start.x);
 	params->delta.y = fabs(end.y - start.y);
 	params->sign.x = (start.x < end.x) ? 1 : -1;
@@ -94,24 +80,12 @@ void	ft_draw_line(mlx_image_t *img, int color, t_DoublePair end,
 	int	safeguard;
 
 	safeguard = 10000;
-	if (!img)
-	{
-		// printf("[ERROR] ft_draw_line: img is NULL\n");
-		return ;
-	}
 	while (((int)params->iter.x != (int)end.x
 			|| (int)params->iter.y != (int)end.y) && safeguard--)
 	{
 		if (params->iter.x >= 0 && params->iter.x < (int)img->width
 			&& params->iter.y >= 0 && params->iter.y < (int)img->height)
-		{
 			mlx_put_pixel(img, (int)params->iter.x, (int)params->iter.y, color);
-		}
-		else
-		{
-			//	printf("[WARNING] ft_draw_line: Out of bounds (x=%.2f,
-			//		y=%.2f)\n",	params->iter.x, params->iter.y);
-		}
 		params->error.y = params->error.x * 2;
 		if (params->error.y > -params->delta.y)
 		{
@@ -126,14 +100,7 @@ void	ft_draw_line(mlx_image_t *img, int color, t_DoublePair end,
 	}
 	if ((int)end.x >= 0 && (int)end.x < (int)img->width && (int)end.y >= 0
 		&& (int)end.y < (int)img->height)
-	{
 		mlx_put_pixel(img, (int)end.x, (int)end.y, color);
-	}
-	else
-	{
-		// printf("[WARNING] ft_draw_line: End point out of bounds (x=%.2f,
-			// y=%.2f)\n", end.x, end.y);
-	}
 }
 
 void	ft_color_line(mlx_image_t *img, int color, t_DoublePair start,
@@ -141,13 +108,6 @@ void	ft_color_line(mlx_image_t *img, int color, t_DoublePair start,
 {
 	t_line_params	params;
 
-	if (!img)
-	{
-		 printf("[ERROR] ft_color_line: img is NULL\n");
-		return ;
-	}
-	printf("[DEBUG] ft_color_line: start=(%.2f, %.2f), end=(%.2f, %.2f)\n",
-		start.x, start.y, end.x, end.y);
 	ft_bresenham_init(&params, start, end);
 	ft_draw_line(img, color, end, &params);
 }
--- draw_ray_mmap.c
+++ draw_ray_mmap.c
@@ -6,58 +6,63 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 21:39:44 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/17 19:23:36 by vmamoten         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:52:12 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "draw.h"
+#include "draw.h"
 
 static void	ft_mlx_highlight_pixel(uint8_t *pixel, uint32_t color)
 {
+	uint8_t		alpha;
+	uint8_t		red;
+	uint8_t		green;
+	uint8_t		blue;
+	uint8_t		newRed;
+	uint8_t		newGreen;
+	uint8_t		newBlue;
+	uint32_t	newColor;
 
-	uint8_t alpha = (color >> 24) & 0xFF;
-	uint8_t red = (color >> 16) & 0xFF;
-	uint8_t green = (color >> 8) & 0xFF;
-	uint8_t blue = color & 0xFF;
-
-	uint8_t newRed = (uint8_t)fmin(255, red * 1.05);
-	uint8_t newGreen = (uint8_t)fmin(255, green * 1.05);
-	uint8_t newBlue = (uint8_t)fmin(255, blue * 1.05);
-
-	uint32_t newColor = (alpha << 24) | (newRed << 16) | (newGreen << 8) | newBlue;
-
-	*(uint32_t*)pixel = newColor;
+	alpha = (color >> 24) & 0xFF;
+	red = (color >> 16) & 0xFF;
+	green = (color >> 8) & 0xFF;
+	blue = color & 0xFF;
+	newRed = (uint8_t)fmin(255, red * 1.05);
+	newGreen = (uint8_t)fmin(255, green * 1.05);
+	newBlue = (uint8_t)fmin(255, blue * 1.05);
+	newColor = (alpha << 24) | (newRed << 16) | (newGreen << 8) | newBlue;
+	*(uint32_t *)pixel = newColor;
 }
 
-void	drawray(t_character *player, mlx_image_t *map_img, t_mini_map *mini_map, t_DoublePair hit)
+void	drawray(t_character *player, mlx_image_t *map_img, t_mini_map *mini_map,
+		t_DoublePair hit)
 {
 	t_IntPair	start;
 	t_IntPair	end;
 
 	if (hit.x < 0 || hit.y < 0)
 		return ;
-		// draw_line_between_pixels(map_img, start, end, 0xFFFFFFFF);
-
-	start.x = (player->pos.x - mini_map->view_port.x) / mini_map->visible_size.x * map_img->width;
-	start.y = (player->pos.y - mini_map->view_port.y) / mini_map->visible_size.y * map_img->height;
-	end.x = (hit.x - mini_map->view_port.x) / mini_map->visible_size.x * map_img->width;
-	end.y = (hit.y - mini_map->view_port.y) / mini_map->visible_size.y * map_img->height;
-	// if (map_img == NULL)
-		// printf("map_img is NULL\n");
+	start.x = (player->pos.x - mini_map->view_port.x) / mini_map->visible_size.x
+		* map_img->width;
+	start.y = (player->pos.y - mini_map->view_port.y) / mini_map->visible_size.y
+		* map_img->height;
+	end.x = (hit.x - mini_map->view_port.x) / mini_map->visible_size.x
+		* map_img->width;
+	end.y = (hit.y - mini_map->view_port.y) / mini_map->visible_size.y
+		* map_img->height;
 	if (end.x >= 0 && end.y >= 0)
-	highlight_line_between_pixels(map_img, start, end);
+		highlight_line_between_pixels(map_img, start, end);
 	draw_line_between_pixels(map_img, start, end, 0xFFFFFFFF);
-	// printf("drew a line\n");
 }
 
-
-void draw_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair end, int color)
+void	draw_line_between_pixels(mlx_image_t *img, t_IntPair start,
+		t_IntPair end, int color)
 {
 	t_IntPair	d;
 	t_IntPair	direction;
 	int			err;
 	int			e2;
-	// printf("image size is x: %d, y: %d\n", img->width, img->height);
+
 	d.y = -abs(end.y - start.y);
 	d.x = abs(end.x - start.x);
 	direction.y = start.y < end.y ? 1 : -1;
@@ -65,7 +70,8 @@ void draw_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair end, 
 	err = d.x + d.y;
 	while (start.x != end.x || start.y != end.y)
 	{
-		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0 && start.y < (int)img->height)
+		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0
+			&& start.y < (int)img->height)
 			mlx_put_pixel(img, start.x, start.y, color);
 		e2 = 2 * err;
 		if (e2 >= d.y)
@@ -79,15 +85,17 @@ void draw_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair end, 
 			start.y += direction.y;
 		}
 	}
-	printf("drew a line\n");
 }
 
-void highlight_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair end)
+void	highlight_line_between_pixels(mlx_image_t *img, t_IntPair start,
+		t_IntPair end)
 {
 	t_IntPair	d;
 	t_IntPair	direction;
 	int			err;
 	int			e2;
+	uint8_t		*pixelstart;
+	uint32_t	currentColor;
 
 	d.y = -abs(end.y - start.y);
 	d.x = abs(end.x - start.x);
@@ -96,10 +104,11 @@ void highlight_line_between_pixels(mlx_image_t *img, t_IntPair start, t_IntPair 
 	err = d.x + d.y;
 	while (start.x != end.x || start.y != end.y)
 	{
-		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0 && start.y < (int)img->height)
+		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0
+			&& start.y < (int)img->height)
 		{
-			uint8_t* pixelstart = &img->pixels[(start.y * img->width + start.x) * BPP];
-			uint32_t currentColor = *(uint32_t*)pixelstart;
+			pixelstart = &img->pixels[(start.y * img->width + start.x) * BPP];
+			currentColor = *(uint32_t *)pixelstart;
 			ft_mlx_highlight_pixel(pixelstart, currentColor);
 		}
 		e2 = 2 * err;
