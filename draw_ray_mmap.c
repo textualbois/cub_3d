ac52f7e3fe9706946be02badb0bf25227b35a411
vmamoten <vladyslav.mamotenko@gmail.com>
2025-02-19 17:16:36 +0100

    refactoring draw_ray_map.c

--- draw_ray_mmap.c
+++ draw_ray_mmap.c
@@ -6,7 +6,7 @@
 /*   By: vmamoten <vmamoten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 21:39:44 by isemin            #+#    #+#             */
-/*   Updated: 2025/02/19 15:12:55 by vmamoten         ###   ########.fr       */
+/*   Updated: 2025/02/19 17:13:18 by vmamoten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 static void	ft_mlx_highlight_pixel(uint8_t *pixel, uint32_t color)
 {
-	uint8_t		channels[4];
+	uint8_t		chan[4];
 	uint32_t	new_color;
 	int			i;
 
-	channels[0] = (color >> 24) & 0xFF;
-	channels[1] = (color >> 16) & 0xFF;
-	channels[2] = (color >> 8) & 0xFF;
-	channels[3] = color & 0xFF;
+	chan[0] = (color >> 24) & 0xFF;
+	chan[1] = (color >> 16) & 0xFF;
+	chan[2] = (color >> 8) & 0xFF;
+	chan[3] = color & 0xFF;
 	i = 1;
 	while (i < 4)
 	{
-		channels[i] = (uint8_t)fmin(255, channels[i] * 1.05);
+		chan[i] = (uint8_t)fmin(255, chan[i] * 1.05);
 		i++;
 	}
-	new_color = (channels[0] << 24) | (channels[1] << 16) | (channels[2] << 8) | channels[3];
+	new_color = (chan[0] << 24) | (chan[1] << 16) | (chan[2] << 8) | chan[3];
 	*(uint32_t *)pixel = new_color;
 }
 
@@ -53,72 +53,95 @@ void	drawray(t_character *player, mlx_image_t *map_img, t_mini_map *mini_map,
 	draw_line_between_pixels(map_img, start, end, 0xFFFFFFFF);
 }
 
+void	draw_line_step(t_IntPair *start, t_IntPair direction, t_IntPair d,
+		int *err)
+{
+	int	e2;
+
+	e2 = 2 * (*err);
+	if (e2 >= d.y)
+	{
+		*err += d.y;
+		start->x += direction.x;
+	}
+	if (e2 <= d.x)
+	{
+		*err += d.x;
+		start->y += direction.y;
+	}
+}
+
 void	draw_line_between_pixels(mlx_image_t *img, t_IntPair start,
 		t_IntPair end, int color)
 {
 	t_IntPair	d;
 	t_IntPair	direction;
 	int			err;
-	int			e2;
 
 	d.y = -abs(end.y - start.y);
 	d.x = abs(end.x - start.x);
-	direction.y = start.y < end.y ? 1 : -1;
-	direction.x = start.x < end.x ? 1 : -1;
+	if (start.y < end.y)
+		direction.y = 1;
+	else
+		direction.y = -1;
+	if (start.x < end.x)
+		direction.x = 1;
+	else
+		direction.x = -1;
 	err = d.x + d.y;
 	while (start.x != end.x || start.y != end.y)
 	{
 		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0
 			&& start.y < (int)img->height)
 			mlx_put_pixel(img, start.x, start.y, color);
-		e2 = 2 * err;
-		if (e2 >= d.y)
-		{
-			err += d.y;
-			start.x += direction.x;
-		}
-		if (e2 <= d.x)
-		{
-			err += d.x;
-			start.y += direction.y;
-		}
+		draw_line_step(&start, direction, d, &err);
+	}
+}
+
+void	highlight_line_step(mlx_image_t *img, t_IntPair *start,
+		t_IntPair direction, int *params)
+{
+	uint8_t		*pixelstart;
+	uint32_t	current_color;
+	int			e2;
+
+	if (start->x >= 0 && start->x < (int)img->width && start->y >= 0
+		&& start->y < (int)img->height)
+	{
+		pixelstart = &img->pixels[(start->y * img->width + start->x) * BPP];
+		current_color = *(uint32_t *)pixelstart;
+		ft_mlx_highlight_pixel(pixelstart, current_color);
+	}
+	e2 = 2 * params[0];
+	if (e2 >= params[2])
+	{
+		params[0] += params[2];
+		start->x += direction.x;
+	}
+	if (e2 <= params[1])
+	{
+		params[0] += params[1];
+		start->y += direction.y;
 	}
 }
 
 void	highlight_line_between_pixels(mlx_image_t *img, t_IntPair start,
 		t_IntPair end)
 {
-	t_IntPair	d;
 	t_IntPair	direction;
-	int			err;
-	int			e2;
-	uint8_t		*pixelstart;
-	uint32_t	currentColor;
+	int			params[3];
 
-	d.y = -abs(end.y - start.y);
-	d.x = abs(end.x - start.x);
-	direction.y = start.y < end.y ? 1 : -1;
-	direction.x = start.x < end.x ? 1 : -1;
-	err = d.x + d.y;
+	params[2] = -abs(end.y - start.y);
+	params[1] = abs(end.x - start.x);
+	if (start.y < end.y)
+		direction.y = 1;
+	else
+		direction.y = -1;
+	if (start.x < end.x)
+		direction.x = 1;
+	else
+		direction.x = -1;
+	params[0] = params[1] + params[2];
 	while (start.x != end.x || start.y != end.y)
-	{
-		if (start.x >= 0 && start.x < (int)img->width && start.y >= 0
-			&& start.y < (int)img->height)
-		{
-			pixelstart = &img->pixels[(start.y * img->width + start.x) * BPP];
-			currentColor = *(uint32_t *)pixelstart;
-			ft_mlx_highlight_pixel(pixelstart, currentColor);
-		}
-		e2 = 2 * err;
-		if (e2 >= d.y)
-		{
-			err += d.y;
-			start.x += direction.x;
-		}
-		if (e2 <= d.x)
-		{
-			err += d.x;
-			start.y += direction.y;
-		}
-	}
+		highlight_line_step(img, &start, direction, params);
 }
